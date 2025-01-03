bc5a03ebf52e018f8d9fa8aedf9d2bb8fbeddc37
Mjahir <hathaliyamahesh2004@gmail.com>
2025-01-03 12:21:57 +0530

    Fix asset and script paths in index.html

--- paste-app/vite.config.js
+++ paste-app/vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  root: 'src', // Set the root to 'src' folder
   plugins: [react()],
+  build: {
+    outDir: '../dist', // Specify output directory outside 'src'
+    emptyOutDir: true,  // Empty the outDir before building
+  },
 })
