9d4cb1e897e2a45ed2df86c25d7747a2f062cc60
Mjahir <hathaliyamahesh2004@gmail.com>
2025-01-03 12:40:29 +0530

    another change

--- paste-app/vite.config.js
+++ paste-app/vite.config.js
@@ -1,12 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  root: 'src', // Set the root to 'src' folder
-  plugins: [react()],
+export default {
   build: {
-    outDir: '../dist', // Specify output directory outside 'src'
-    emptyOutDir: true,  // Empty the outDir before building
-  },
-})
+    outDir: 'dist',  // Vercel expects the built files to be in the 'dist' folder
+    emptyOutDir: true,
+  }
+};
